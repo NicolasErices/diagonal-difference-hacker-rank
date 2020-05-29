98d6c9491788ab518f43576abf57f78ee6eefff6
carlos erices mendez <carlos.erices@bci.cl>
2020-05-29 19:00:13 -0400

    FIX | correction name variable

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -1,10 +1,14 @@
-const arr = [
-    [-10, 3, 0, 5, -4],
-    [2, -1, 0, 2, -8],
-    [9, -1, -5, 6, 0],
-    [9, -7, -4, 8, -2],
-    [3, 7, 8, -5, 0],
-];
+
+//this is an example matrix since in hacker rank they test
+//multiple quadratic arrays of 100x100 or less or more with
+//negative deaths as positive
+// const arr = [
+//     [-10, 3, 0, 5, -4],
+//     [2, -1, 0, 2, -8],
+//     [9, -1, -5, 6, 0],
+//     [9, -7, -4, 8, -2],
+//     [3, 7, 8, -5, 0],
+// ];
 
 function matrixTwoDimension(arr){
     let diagonalLeft = [];
@@ -20,8 +24,8 @@ function matrixTwoDimension(arr){
     let sumRightDiagonal = diagonalRight.reduce((acc, cur) => {
         return acc + cur
     })
-    let resultado = Math.abs(sumRightDiagonal - sumLeftDiagonal)
-    return resultado
+    let Outcome = Math.abs(sumRightDiagonal - sumLeftDiagonal)
+    return Outcome
 }
 
 matrixTwoDimension(arr)
